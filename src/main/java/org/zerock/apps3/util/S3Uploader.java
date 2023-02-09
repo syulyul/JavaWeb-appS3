3c3ff9c55447b9ef8dbc378eb00475a912f362d8
syulyul <syul9800@gmail.com>
2023-02-09 22:41:14 +0900

    98% finished

--- src/main/java/org/zerock/apps3/util/S3Uploader.java
+++ src/main/java/org/zerock/apps3/util/S3Uploader.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
